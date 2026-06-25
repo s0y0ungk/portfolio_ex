47163abcdab6e093d8bb5adc01c6e708b281e3ac
s0y0ungk <soyo2039@gmail.com>
2026-06-25 17:07:16 +0900

    3단계

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,15 @@
-function Nav() {
+function Nav({ data }) {
   console.log("Nav render");
+
+  const lists = data.map(work => (
+    <li key={work.id}>
+      <a href="">{work.title}</a>
+    </li>
+  ));
 
   return (
     <nav>
-      <ul>
-        <li>
-          <a href="">work 1</a>
-        </li>
-        <li>
-          <a href="">work 2</a>
-        </li>
-        <li>
-          <a href="">work 3</a>
-        </li>
-        <li>
-          <a href="">work 4</a>
-        </li>
-      </ul>
+      <ul>{lists}</ul>
     </nav>
   );
 }
